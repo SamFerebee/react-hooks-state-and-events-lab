b6d4b30e1943895799df53c6280cd62c389845ea
Sam Ferebee <sam.ferebee@gmail.com>
2021-03-19 13:47:55 -0400

    commit tree 2306851a244b881914db21858a92ab6efcfa5a69

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [cartOrNot, setCartOrNot] = useState("")
+  const addToCart = () =>{
+    setCartOrNot("in-cart")
+  }
   return (
-    <li className="">
+    <li className={cartOrNot}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCart}className="add">Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const handleChange = e => {
+    setSelectedCategory(e.target.value)
+  } 
+  const allItems = items.map(item => <Item key={item.id} name={item.name} category={item.category} />)
+  const filtered = items.filter(item => item.category === selectedCategory )
+  const filteredItems = filtered.map(item => <Item key={item.id} name={item.name} category={item.category} />)
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +20,9 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {/* {items.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} /> */}
+          {selectedCategory != "All" ? filteredItems : allItems}
       </ul>
     </div>
   );
